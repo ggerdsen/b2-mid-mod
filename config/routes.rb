38c897f4341b386284ad7a2ce7e17189da2a5f4e
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-09-21 10:33:24 -0600

    Create route for passenger removal from flight

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 
   get "/flights", to: "flights#index"
   get "/airlines/:airline_id", to: "airlines#show"
+  delete "/flights/:flight_id/passengers/:passenger_id", to: "flights#update"
 end
